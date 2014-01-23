1802fac64f377c79cc8e1caa1a069f3c7736675e
wshcdr <wshcdr@eyou.com>
2014-01-23 18:03:07 +0800

    linklist
    linklist and testcase

---
+++ NSLinkList/NSLinkList/NSPointsList.h
@@ -0,0 +1,34 @@
+//
+//  NSPointsList.h
+//  mylist002
+//
+//  Created by apple on 14-1-21.
+//  Copyright (c) 2014年      . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CGPointEx.h"
+#import "LPtNode.h"
+
+
+
+@interface NSPointsList : NSObject{
+    
+    LPtNode *first;
+    LPtNode *last;
+    
+    unsigned int size;
+    
+}
+
+- (id)init;                                 // init an empty list
+- (void)pushBack:(CGPointEx*)anObject;              // add an object to back of list
+- (int)size;                                // how many objects are stored
+- (CGPointEx*)getPt:(const int)idx;
+
+@property (nonatomic, strong) LPtNode *first;
+@property (nonatomic, strong) LPtNode *last;
+
+@end
+
+LPtNode * LPtNodeMake(CGPointEx* obj, LPtNode *next, LPtNode *prev);
---
+++ NSLinkList/NSLinkList/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NSLinkList
+//
+//  Created by apple on 14-1-23.
+//  Copyright (c) 2014年     . All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
